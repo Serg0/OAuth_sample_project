fcd677e36a00eb1ff431d7280d592af3a6233f59
unknown <sergnad@mail.ru>
2012-12-10 18:53:54 +0200

    v2 Get Token in process

--- src/com/example/oauth_sample_project/OAuth.java
+++ src/com/example/oauth_sample_project/OAuth.java
@@ -1,11 +1,12 @@
 package com.example.oauth_sample_project;
 
+import java.io.IOException;
+
 import android.accounts.AccountManager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.Application;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -19,114 +20,147 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
-import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.ConnectionResult;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
 public class OAuth extends Activity {
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] {
-			"foo@example.com:hello", "bar@example.com:world" };
-
-	/**
-	 * The default email to populate the email field with.
-	 */
-	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
-
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private UserLoginTask mAuthTask = null;
-
-	// Values for email and password at the time of the login attempt.
-	private String mEmail;
-	private String mPassword;
-
-	// UI references.
-	private EditText mEmailView;
-	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
-
+	private final static String G_PLUS_SCOPE = "oauth2:https://www.googleapis.com/auth/plus.me";
+	private final static String USERINFO_SCOPE = "https://www.googleapis.com/auth/userinfo.profile";
+	private final static String SCOPES = G_PLUS_SCOPE + " " + USERINFO_SCOPE;
 	private int ReqCode;
 
 	public int SOME_REQUEST_CODE = 1;
 
 	private String accountName;
+
+	private String accountType;
+	private String token;
+
+	TextView txtResult;
 	
+	public OAuth Instance;
+	public String result = "initial Token";
+	class getTokenTask extends AsyncTask<Void, Void, String> {
+		
+		@Override
+		protected String doInBackground(Void... noargs) {
+//			try {
+//				result = getToken();
+//				txtResult.setText(result);
+//				return result;
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			result = "No Token";
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(String _result) {
+			txtResult.setText(_result);
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_oauth);
+		TextView txtResult = (TextView) findViewById(R.id.textView1);
+ Instance = this;
+		ReqCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getApplicationContext());
 
-		// Set up the login form.
-		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
-		mEmailView = (EditText) findViewById(R.id.email);
-		mEmailView.setText(mEmail);
+		findViewById(R.id.button1).setOnClickListener(
+				new View.OnClickListener() {
 
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
+					public void onClick(View v) {
+
+						Toast.makeText(getApplication(),
+								Integer.toString(ReqCode), Toast.LENGTH_LONG)
+								.show();
+
+						Intent intent = AccountPicker.newChooseAccountIntent(
+								null, null, new String[] { "com.google" },
+								false, null, null, null, null);
+						startActivityForResult(intent, SOME_REQUEST_CODE);
+						// Toast.makeText(getApplication(), "Account chosen " +
+						// accountName, Toast.LENGTH_LONG).show();
+						// Toast.makeText(getApplication(),
+						// "Account TYPE chosen " + accountType,
+						// Toast.LENGTH_LONG).show();
+
 					}
 				});
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
-		
-		findViewById(R.id.sign_in_button).setOnClickListener(
+		findViewById(R.id.button2).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
-					public void onClick(View view) {
-						attemptLogin();
+					public void onClick(View v) {
+						Toast.makeText(getApplication(),
+								"Account chosen " + accountName,
+								Toast.LENGTH_LONG).show();
+						Toast.makeText(getApplication(),
+								"Account TYPE chosen " + accountType,
+								Toast.LENGTH_LONG).show();
+
+//						try {
+//							Toast.makeText(getApplication(),
+//									"Token " + getToken(), Toast.LENGTH_LONG)
+//									.show();
+							getTokenTask _getTokenTask = new getTokenTask();
+							_getTokenTask.execute();
+							
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+
 					}
 				});
-	 ReqCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-	 
-		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-			
-			
-
-			@Override
-			public void onClick(View v) {
-				
-				Toast.makeText(getApplication(), Integer.toString(ReqCode), Toast.LENGTH_LONG).show();
-				
-				Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"},
-				         false, null, null, null, null);
-				 startActivityForResult(intent, SOME_REQUEST_CODE );
-				 Toast.makeText(getApplication(), "Account chosen " + accountName, Toast.LENGTH_LONG).show();
-				
-			}
-		});
 	}
 
+	public String getToken() throws InterruptedException {
 
-	 protected void onActivityResult(final int requestCode, final int resultCode,
-	         final Intent data) {
-	     if (requestCode == SOME_REQUEST_CODE && resultCode == RESULT_OK) {
-	          accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-	         
-	   }
-	 }
+		
+		 Runnable runnable = new Runnable() {
+		
+		 @Override
+		 public void run() {
+		try {
+			token = GoogleAuthUtil.getToken(Instance.getApplicationContext(),
+					accountName, SCOPES);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (GoogleAuthException e) {
+			e.printStackTrace();
+		}
+
+		 }};
+		 Thread thread;
+		 thread = new Thread(runnable);
+		 thread.start();
+		 thread.join();
+
+		return token;
+	}
+
+	protected void onActivityResult(final int requestCode,
+			final int resultCode, final Intent data) {
+		if (requestCode == SOME_REQUEST_CODE && resultCode == RESULT_OK) {
+			accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+			accountType = data.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE);
+
+		}
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -134,150 +168,4 @@ public class OAuth extends Activity {
 		return true;
 	}
 
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		if (mAuthTask != null) {
-			return;
-		}
-
-		// Reset errors.
-		mEmailView.setError(null);
-		mPasswordView.setError(null);
-
-		// Store values at the time of the login attempt.
-		mEmail = mEmailView.getText().toString();
-		mPassword = mPasswordView.getText().toString();
-
-		boolean cancel = false;
-		View focusView = null;
-
-		// Check for a valid password.
-		if (TextUtils.isEmpty(mPassword)) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			focusView = mPasswordView;
-			cancel = true;
-		} else if (mPassword.length() < 4) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(mEmail)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
-			cancel = true;
-		} else if (!mEmail.contains("@")) {
-			mEmailView.setError(getString(R.string.error_invalid_email));
-			focusView = mEmailView;
-			cancel = true;
-		}
-
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView.requestFocus();
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-			showProgress(true);
-			mAuthTask = new UserLoginTask();
-			mAuthTask.execute((Void) null);
-		}
-	}
-
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
-
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
-
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
-
-	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
-	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			// TODO: attempt authentication against a network service.
-
-			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
-			}
-
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
-			}
-
-			// TODO: register the new account here.
-			return true;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-			mAuthTask = null;
-			showProgress(false);
-
-			if (success) {
-				finish();
-			} else {
-				mPasswordView
-						.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
-			}
-		}
-
-		@Override
-		protected void onCancelled() {
-			mAuthTask = null;
-			showProgress(false);
-		}
-	}
 }
